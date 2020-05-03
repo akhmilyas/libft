6047c357dacf8ad2b5ffdb004c9475bb9032f460
Admin <admin@Mac.local>
2020-05-03 16:09:48 +0300

    norm strjoin

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: mstoops <mstoops@student.21-school.ru>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/03 13:35:17 by mstoops           #+#    #+#             */
-/*   Updated: 2020/05/03 15:45:07 by mstoops          ###   ########.fr       */
+/*   Updated: 2020/05/03 16:08:45 by mstoops          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,28 @@
 
 char	*ft_strjoin(char const *s1, char const *s2)
 {
-	char*	str;
+	char	*str;
 
-	if (!s1 || !s2)
+	if (!s1 && !s2)
 		return (NULL);
-	if (!(str = (char*)malloc(ft_strlen(s1) + ft_strlen(s2) + 1)))
-		return (NULL);
-	ft_strlcpy(str, s1, ft_strlen(s1) + 1);
-	ft_strlcat(str, s2, ft_strlen(s1) + ft_strlen(s2) + 1);
-	return str;
+	else if (!s1)
+	{
+		if (!(str = (char*)malloc(ft_strlen(s2) + 1)))
+			return (NULL);
+		ft_strlcpy(str, s2, ft_strlen(s2) + 1);
+	}
+	else if (!s2)
+	{
+		if (!(str = (char*)malloc(ft_strlen(s1) + 1)))
+			return (NULL);
+		ft_strlcpy(str, s1, ft_strlen(s1) + 1);
+	}
+	else
+	{
+		if (!(str = (char*)malloc(ft_strlen(s1) + ft_strlen(s2) + 1)))
+			return (NULL);
+		ft_strlcpy(str, s1, ft_strlen(s1) + 1);
+		ft_strlcat(str, s2, ft_strlen(s1) + ft_strlen(s2) + 1);
+	}
+	return (str);
 }
