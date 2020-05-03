7359d07346981b025c861a56f32da45ac08bb596
Admin <admin@Mac.local>
2020-05-04 02:32:44 +0300

    fix strnstr

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: mstoops <mstoops@student.21-school.ru>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/02 20:28:37 by mstoops           #+#    #+#             */
-/*   Updated: 2020/05/03 02:54:05 by mstoops          ###   ########.fr       */
+/*   Updated: 2020/05/04 02:32:23 by mstoops          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,13 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 	size_t	i;
 
 	i = 0;
-	if (!haystack)
-		*(char*)haystack = 5;
+	if (!haystack || !needle)
+		return (NULL);
 	if (ft_strlen(needle) > len)
 		return (NULL);
 	if (ft_strlen(needle) == 0 || len == 0)
 		return ((char*)haystack);
-	while (i <= len - ft_strlen(needle))
+	while (i + ft_strlen(needle) <= len)
 	{
 		if (haystack[i] == needle[0] &&
 			(ft_strncmp(haystack + i, needle, ft_strlen(needle)) == 0))
