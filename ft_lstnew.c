26875e4880ff702618e995bf1c0ce8f7cfc3a86d
mstoops <mstoops@student.21-school.ru>
2020-05-14 16:36:32 +0300

    lstnew

---
+++ ft_lstnew.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mstoops <mstoops@student.21-school.ru>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/05/14 16:33:46 by mstoops           #+#    #+#             */
+/*   Updated: 2020/05/14 16:36:07 by mstoops          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstnew(void *content)
+{
+	t_list	*node;
+
+	if (!(node = malloc(sizeof(t_list))))
+		return (NULL);
+	node->content = content;
+	node->next = NULL;
+	return(node);
+}
